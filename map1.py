a6f74f4ef2cd80227ef2858ba91582d2198a6b8a
Christian Soto <christian.j.sotoparra@gmail.com>
2021-10-10 23:02:41 -0700

    Implemented list of volcanoes

--- map1.py
+++ map1.py
@@ -1,8 +1,16 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
 map = folium.Map(location=[35.70387918204649, 139.79352861911002], tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln in zip(lat,lon):
+	map.add_child(folium.Marker(location=[lt,ln], popup="Hi, I'm a Marker", icon=folium.Icon(color="blue")))
 
 for coordinates in [[35.70387918204649, 139.79352861911002]]:
 	map.add_child(folium.Marker(location=coordinates, popup="Good Bar!", icon=folium.Icon(color='red')))
