91a2808473e9c2751c49f223fdefdd7b01ac6979
Christian Soto <christian.j.sotoparra@gmail.com>
2021-10-10 22:28:22 -0700

    Added for loop for extraction of coordinates

--- map1.py
+++ map1.py
@@ -3,7 +3,9 @@ import folium
 map = folium.Map(location=[35.70387918204649, 139.79352861911002], tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-map.add_child(folium.Marker(location=[35.70387918204649, 139.79352861911002], popup="Good Bar!", icon=folium.Icon(color='red')))
+
+for coordinates in [[35.70387918204649, 139.79352861911002]]:
+	map.add_child(folium.Marker(location=coordinates, popup="Good Bar!", icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
