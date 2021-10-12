ada50538ca708c8e5f40635449f54e88c7d75d2b
Christian Soto <christian.j.sotoparra@gmail.com>
2021-10-11 22:43:01 -0700

    Added circle markers and color elvation identifers

--- map1.py
+++ map1.py
@@ -5,16 +5,35 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
+
 
 map = folium.Map(location=[35.70387918204649, 139.79352861911002], tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, el in zip(lat,lon,elev):
-	map.add_child(folium.Marker(location=[lt,ln], popup=str(el)+ " m", icon=folium.Icon(color="blue")))
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 
-map.add_child(folium.Marker(location=[35.70387918204649, 139.79352861911002], popup="Good Bar!", icon=folium.Icon(color='red')))
+
+fg = folium.FeatureGroup(name = "My Map")
+colr = " "
+
+for lt, ln, el, name in zip(lat, lon, elev, name):
+
+	if el <= 1000:
+		colr = "red"
+	elif el > 1000 and el <= 2000:
+		colr = "blue"
+	elif el > 2000 and el <= 3000:
+		colr = "green"
+	else:
+		colr = "orange"
+
+	iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+	fg.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=folium.Popup(iframe), fill_color=colr, color= 'grey', fill_opacity=.7))
 
 map.add_child(fg)
-
-map.save("Map1.html")
+map.save("Map_html_popup_advanced.html")
